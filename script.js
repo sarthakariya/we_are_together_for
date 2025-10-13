f0c3bdac71093648e6777b3582f05deb957e169e
S@rth@k ;) <26sarthakbasu@gmail.com>
2025-10-14 00:57:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -94,7 +94,7 @@ function updateTimer() {
 
 // Optimization: We still use setInterval for the clock update (which is once per second) 
 // but the canvas animation runs using requestAnimationFrame, which is smooth.
-setInterval(updateTimer, 993); 
+setInterval(updateTimer, 997); 
 updateTimer();
 
 // --- 2. PET NAME ANIMATION ---
